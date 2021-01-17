c5e7ee88e09178526c2e8bcc38ca7fadf15ec6ab
Rajkumar Lolla <rajkumarlolla@yahoo.com>
2021-01-18 01:30:57 +0530

    Implemented Part A of the Assignment
    a) Implemented Sign up and Sign in modal popups
    b) created Bloglist.html
    c) Implemented Create Post and Navigate to Block List requirements.
    d) updated index.js with functions to handle create post and navigate methods.

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,35 @@
 function showModalSignIn(){
-    var modal = document.getElementById("signin-modal");
+    var modal = document.getElementById("modal-signin");
     modal.style.display = "flex";
   }
   
   function closeSignInModal(){
-    var modal = document.getElementById("signin-modal");
+    var modal = document.getElementById("modal-signin");
     modal.style.display = "none";
   }
   
   function showModalSignUp(){
-    var modal = document.getElementById("signup-modal");
+    var modal = document.getElementById("modal-signup");
     modal.style.display = "flex";
   }
   
   function closeSignUpModal(){
-    var modal = document.getElementById("signup-modal");
+    var modal = document.getElementById("modal-signup");
     modal.style.display = "none";
   }
+
+  function showModalCreatePost(){
+    alert('Entered');
+    var modal = document.getElementById("modal-createPost");
+    modal.style.display = "flex";
+  }
+  
+  function closeModalCreatePost(){
+    var modal = document.getElementById("modal-createPost");
+    modal.style.display = "none";
+  }
+  
+  function navigateToBlogList(){
+    location.href = "html/bloglist.html"
+  }
+  
