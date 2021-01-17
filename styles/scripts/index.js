20b04360b38f915b457f875d1b39ddf7e90422f6
Rajkumar Lolla <rajkumarlolla@yahoo.com>
2021-01-18 00:43:28 +0530

    Added Index.js file to handle the sign in and sign up onclick events

---
+++ styles/scripts/index.js
@@ -0,0 +1,19 @@
+function showModalSignIn(){
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "flex";
+  }
+  
+  function closeSignInModal(){
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "none";
+  }
+  
+  function showModalSignUp(){
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "flex";
+  }
+  
+  function closeSignUpModal(){
+    var modal = document.getElementById("signup-modal");
+    modal.style.display = "none";
+  }
